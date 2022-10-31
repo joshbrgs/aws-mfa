c1c5fec962443fdde58e1c18add1c4d8bd47f906
Josh Burgess <77697539+jlifts@users.noreply.github.com>
2022-10-31 15:19:56 -0400

    Add files via upload

---
+++ aws/aws.go
@@ -0,0 +1,163 @@
+package aws
+
+import (
+	"fmt"
+	"jlifts/aws-mfa/color"
+	"os/exec"
+	"strings"
+	"time"
+)
+
+func AWSSetCommand(varname string, value string, verboseFl bool, mfaProfile string) {
+	var app string = "aws"
+	var configureCommand string = "configure"
+	var setCommand string = "set"
+	var arg1 string = "--profile"
+	var arg1Val string = mfaProfile
+	var arg2 string = "--output"
+	var arg2Val string = "json"
+	var exampleCommand string = app + " " + configureCommand + " " + setCommand + " " + varname + " " + value + " " + arg1 + " " + arg1Val + " " + arg2 + " " + arg2Val
+
+	if verboseFl {
+		fmt.Println(exampleCommand)
+	}
+
+	out, err := exec.Command(app, configureCommand, setCommand, varname, value, arg1, arg1Val, arg2, arg2Val).CombinedOutput()
+
+	if err != nil {
+		fmt.Println(color.Red + string(out) + color.Reset)
+		panic(err)
+	}
+
+	if out != nil && verboseFl {
+		fmt.Printf("Set AWS Cred: %s ✔\n", varname)
+	}
+}
+
+func AWSSessionCommand(arn string, code string, verboseFl bool, defaultProfile string) (result string) {
+	var app string = "aws"
+	command := "sts"
+	command1 := "get-session-token"
+	arg1 := "--serial-number"
+	arg1Val := arn
+	arg2 := "--token-code"
+	arg2Val := code
+	arg3 := "--profile"
+	arg3Val := defaultProfile
+	arg4 := "--output"
+	arg4Val := "json"
+	execExample := app + " " + command + " " + command1 + " " + arg1 + " " + arg1Val + " " + arg2 + " " + arg2Val + " " + arg3 + " " + arg3Val + " " + arg4 + " " + arg4Val
+
+	if verboseFl {
+		fmt.Println(execExample)
+	}
+	out, err := exec.Command(app, command, command1, arg1, arg1Val, arg2, arg2Val, arg3, arg3Val, arg4, arg4Val).CombinedOutput()
+
+	if err != nil {
+		fmt.Println(color.Red + string(out) + color.Reset)
+		panic(err)
+	}
+
+	result = string(out)
+
+	return result
+}
+
+func CreateAwsMfaProfile(mfaProfile string) {
+	fmt.Printf("Creating %s profile...\n", mfaProfile)
+	profile := "profile." + mfaProfile + ".region"
+	out, err := exec.Command("aws", "configure", "set", profile, "us-east-1").CombinedOutput()
+
+	if err != nil {
+		fmt.Println(color.Red + string(out) + color.Reset)
+		panic(err)
+	}
+
+	profile = "profile." + mfaProfile + ".output"
+	out, err = exec.Command("aws", "configure", "set", profile, "json").CombinedOutput()
+
+	if err != nil {
+		fmt.Println(color.Red + string(out) + color.Reset)
+		panic(err)
+	}
+
+	if out != nil {
+		fmt.Printf("%s profile created ✔\n", mfaProfile)
+	}
+}
+
+func MfaProfileCheck(mfaProfile string) bool {
+	out, err := exec.Command("aws", "configure", "list", "--profile", mfaProfile).CombinedOutput()
+
+	if err != nil {
+		fmt.Println("We will create a AWS CLI profile for your MFA Creds")
+	}
+
+	var profileCheck = string(out)
+
+	if strings.Contains(profileCheck, "could not be found") {
+		CreateAwsMfaProfile(mfaProfile)
+	}
+
+	// get expiration, if expired, return and say sesssion is still active
+	var live bool = CheckSessionExpiration(mfaProfile)
+
+	if live {
+		return true
+	}
+
+	return false
+}
+
+func KubeConfig(verboseFl bool, mfaProfile string) {
+	var app string = "aws"
+
+	fmt.Println("Region Code:")
+	var region string
+	fmt.Scanln(&region)
+
+	fmt.Println("What is your Cluster Name:")
+	var cluster string
+	fmt.Scanln(&cluster)
+
+	if verboseFl {
+		fmt.Println(app + " " + "eks" + " " + "update-kubeconfig" + " " + "--region" + " " + region + " " + "--name" + " " + cluster + " " + "--profile" + " " + mfaProfile)
+	}
+	out, err := exec.Command(app, "eks", "update-kubeconfig", "--region", region, "--name", cluster, "--profile", mfaProfile).CombinedOutput()
+
+	if err != nil {
+		fmt.Println(color.Red + string(out) + color.Reset)
+		panic(err)
+	}
+
+	if out != nil {
+		fmt.Println("Kube Config Updated ✔")
+	}
+}
+
+func CheckSessionExpiration(mfaProfile string) bool {
+	profile := "profile." + mfaProfile + ".expiration"
+	out, err := exec.Command("aws", "configure", "get", profile).CombinedOutput()
+
+	if err != nil {
+		fmt.Println("**Ignore if this is your first time**")
+		fmt.Println("Error in reading token expiration date")
+	}
+
+	var expiresAt string = string(out)
+	expiresAt = strings.ReplaceAll(expiresAt, "\x0d\x0a", "")
+	var now time.Time = time.Now()
+	t, err := time.Parse(time.RFC3339, expiresAt)
+
+	if err != nil {
+		fmt.Println(color.Red + err.Error() + color.Reset)
+	}
+
+	if now.Before(t) {
+		fmt.Printf("Time is %s\n", now)
+		fmt.Println("Session Token is still valid")
+		return true
+	}
+
+	return false
+}
---
+++ utils/utils.go
@@ -0,0 +1,116 @@
+package utils
+
+import (
+	"encoding/json"
+	"fmt"
+	"jlifts/aws-mfa/aws"
+	"jlifts/aws-mfa/color"
+	"os"
+	"os/exec"
+)
+
+type CredentialsType struct {
+	SecretAccessKey string `json:"SecretAccessKey"`
+	SessionToken    string `json:"SessionToken"`
+	Expiration      string `json:"Expiration"`
+	AccessKeyId     string `json:"AccessKeyId"`
+}
+
+type AwsSession struct {
+	Credentials CredentialsType `json:"Credentials"`
+}
+
+type MfaDevicesType struct {
+	UserName     string `json:"UserName"`
+	SerialNumber string `json:"SerialNumber"`
+	EnableDate   string `json:"EnableDate"`
+}
+
+type AwsMfaDevices struct {
+	MfaDevices []MfaDevicesType `json:"MFADevices"`
+}
+
+/* Configures the AWS SEssion utilizing the AWS package functions with the arguments of the arn, mfa code, and optional verbose flag*/
+func ConfigureSession(arn string, code string, verboseFL bool, defaultProfile string, mfaProfile string) {
+	// build aws mfa get session token by mfa command
+	result := aws.AWSSessionCommand(arn, code, verboseFL, defaultProfile)
+
+	//convert the result to the json structure for go
+	var convertedResult AwsSession
+
+	decodeErr := json.Unmarshal([]byte(result), &convertedResult)
+
+	if decodeErr != nil {
+		fmt.Println(color.Red + "JSON decode error!" + color.Reset)
+		panic(decodeErr)
+	}
+
+	if verboseFL {
+		fmt.Printf("Expires at: %s\n", convertedResult.Credentials.Expiration)
+	}
+
+	var varname string = "aws_access_key_id"
+	var value = convertedResult.Credentials.AccessKeyId
+	aws.AWSSetCommand(varname, value, verboseFL, mfaProfile)
+
+	varname = "aws_secret_access_key"
+	value = convertedResult.Credentials.SecretAccessKey
+	aws.AWSSetCommand(varname, value, verboseFL, mfaProfile)
+
+	varname = "aws_session_token"
+	value = convertedResult.Credentials.SessionToken
+	aws.AWSSetCommand(varname, value, verboseFL, mfaProfile)
+
+	varname = "expiration"
+	value = convertedResult.Credentials.Expiration
+	aws.AWSSetCommand(varname, value, verboseFL, mfaProfile)
+}
+
+/* Retrieves the Arn via username or an environment variable and returns it as a string */
+func GetARN(defaultProfile string) (arn string) {
+	app := "aws"
+
+	// If arn device has not been set
+	arn, ok := os.LookupEnv("MFA_ARN")
+	name, alright := os.LookupEnv("AWS_USER")
+
+	if !ok {
+		if !alright {
+			fmt.Println("What is your AWS username?:")
+			fmt.Scanln(&name)
+			fmt.Printf("For an easier Auth use this command on windows cmd: set AWS_USER=%s\n", name)
+		}
+
+		out, err := exec.Command(app, "iam", "list-mfa-devices", "--user-name", name, "--profile", defaultProfile, "--output", "json").CombinedOutput()
+
+		if err != nil {
+			fmt.Println(color.Red + string(out) + color.Reset)
+			fmt.Println("Do you have MFA set up in AWS?")
+			panic(err)
+		}
+
+		//convert the result to the json structure for go
+		var result string = string(out)
+		var convertedMFA AwsMfaDevices
+
+		decodeErr := json.Unmarshal([]byte(result), &convertedMFA)
+
+		if decodeErr != nil {
+			fmt.Println(color.Red + "JSON decode error!" + color.Reset)
+			panic(decodeErr)
+		}
+
+		arn = convertedMFA.MfaDevices[0].SerialNumber
+
+		if err != nil {
+			fmt.Println(color.Purple + "What is your mfa devices arn?:" + color.Reset)
+			fmt.Println()
+
+			fmt.Scanln(&arn)
+
+			fmt.Printf("For an easier Auth use this command on windows cmd: set MFA_ARN=%s", arn)
+		}
+	}
+
+	return arn
+}
